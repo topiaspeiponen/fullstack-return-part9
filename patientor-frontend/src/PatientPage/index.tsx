72ead9576c8a65169df5615b01d067b772106d21
topiaspe <topiaspe@metropolia.fi>
2022-05-23 15:31:05 +0300

    exercise 9.18 patientor

---
+++ patientor-frontend/src/PatientPage/index.tsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { updatePatient, useStateValue } from "../state";
+
+
+const PatientPage = () => {
+    const { id } = useParams<{ id: string}>();
+
+    const [{ patients }, dispatch] = useStateValue();
+
+    const patientToView = Object.values(patients).find(patient => patient.id === id);
+
+    useEffect(() => {
+        try {
+            void axios.get<Patient>(
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                `${apiBaseUrl}/patients/${id}`
+            ).then((response) => {
+                dispatch(updatePatient(response.data));
+            });
+        } catch(e: unknown) {
+            if (axios.isAxiosError(e)) {
+                console.error(e?.response?.data || "Unrecognized axios error");
+              } else {
+                console.error("Unknown error", e);
+              }
+        }
+    }, []);
+
+    if (!patientToView) {
+        return null;
+    } else {
+        return (
+            <div>
+                <h2>
+                    {patientToView.name}
+                </h2>
+                <div>
+                    gender: {patientToView.gender}
+                </div>
+                <div>
+                    ssn: {patientToView.ssn}
+                </div>
+                <div>
+                    occupation: {patientToView.occupation}
+                </div>
+            </div>
+        );
+    }
+};
+
+export default PatientPage;
